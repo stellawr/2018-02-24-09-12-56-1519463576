4382fe9172fdf5df5649251ea52dfde5979fbab8
stellawr <564048398@qq.com>
2018-06-02 21:49:26 +0800

    完成第二个

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function collect_last_element(collection) {
-  //在这里写入代码
+  let last;
+  let length=collection.length;
+  last=collection[length-1];
+  return last;
+
 }
 
 module.exports = collect_last_element;
