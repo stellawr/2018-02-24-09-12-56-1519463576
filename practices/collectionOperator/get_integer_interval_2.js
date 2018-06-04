8c2ed16d80f5156734960d2fb750e2e842520bb4
stellawr <564048398@qq.com>
2018-06-04 14:48:45 +0800

    完成第三个

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,25 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  let result=[];
+  if(number_a<number_b){
+    let j=0;
+  for( let i = number_a; i<=number_b; i++)
+    if(i%2==0)
+    {result[j]=i;
+      j++;
+    }
+  }
+  if(number_a>number_b){
+    let j=0;
+    for(let i=number_a;i>=number_b;i--)
+      if(i%2==0)
+      { result[j]=i;
+        j++;}
+  }
+  if(number_a==number_b && number_a%2==0)
+    result=[number_a];
+  return result;
 }
 
 module.exports = get_integer_interval_2;
