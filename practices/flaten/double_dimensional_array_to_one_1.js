5c1ccc66ad84447a64b4332b6c66a4ae36f3129e
stellawr <564048398@qq.com>
2018-06-05 21:54:14 +0800

    完成flaten

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -2,7 +2,15 @@
 
 function double_to_one(collection) {
 
-  //在这里写入代码
+  let result=[];
+  for(let i=0;i<collection.length;i++){
+    if(collection[i].length==undefined)
+      result.push(collection[i]);
+    else{
+      for(let j=0;j<collection[i].length;j++)
+         result.push(collection[i][j]);
+    }
+  }
+  return result;
 }
-
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -2,7 +2,22 @@
 
 function double_to_one(collection) {
 
-  //在这里写入代码
+  let result=[];
+  for(let i=0;i<collection.length;i++){
+    if(collection[i].length==undefined)
+      result.push(collection[i]);
+    else{
+      for(let j=0;j<collection[i].length;j++)
+        result.push(collection[i][j]);
+    }
+  }
+  for(let i=result.length;i>=0;i-- ){
+    for(let j=0;j<i;j++)
+      if(result[i]==result[j])
+        result.splice(i,1);
+  }
+
+  return result;
 }
 
 module.exports = double_to_one;
